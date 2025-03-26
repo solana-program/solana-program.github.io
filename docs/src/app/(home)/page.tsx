62554a9fa58cafa75a445a3bb8c2f4878e0995bb
Jon C <me@jonc.dev>
2025-03-26 01:26:02 +0100

    Fixup various things
    * Update the language on the front page to point to the old page
    * Add the logo

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -3,26 +3,50 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Solana Program (Library)</h1>
+      <h1 className="mb-4 text-2xl font-bold">Welcome to the new home of the Solana Program Library!</h1>
       <h3 className="mb-4 text-2x1">
-        The <a href="https://github.com/solana-program">Solana Program</a> organization
-        contains a collection of on-chain programs targeting the <a href="https://www.anza.xyz/blog/the-solana-ebpf-virtual-machine">Solana Virtual Machine</a>.
+        The{' '}
+        <Link
+          href="https://github.com/solana-program"
+          className="text-fd-foreground font-semibold underline"
+        >
+          Solana Program
+        </Link> organization
+        contains a collection of on-chain programs targeting the{' '}
+        <Link
+          href="https://www.anza.xyz/blog/the-solana-ebpf-virtual-machine"
+          className="text-fd-foreground font-semibold underline"
+        >
+          Solana Virtual Machine
+        </Link>.
       </h3>
       <h3 className="mb-4 text-2x1">
-        These programs are tested against the <a href="https://github.com/anza-xyz/agave/">Agave</a> implementation of the SVM and deployed to Solana mainnet-beta.
-      </h3>
-      <h3 className="mb-4 text-2x1">
-        All programs are currently maintained by <a href="https://www.anza.xyz">Anza</a>.
+        All programs are currently maintained by{' '}
+        <Link
+          href="https://www.anza.xyz"
+          className="text-fd-foreground font-semibold underline"
+        >
+          Anza
+        </Link>.
       </h3>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        Go to{' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
         </Link>{' '}
-        to see program-specific documentation.
+        for up-to-date program-specific documentation.
+      </p>
+      <p className="text-fd-muted-foreground">
+        Old docs still exist at{' '}
+        <Link
+          href="https://spl.solana.com"
+          className="text-fd-foreground font-semibold underline"
+        >
+          https://spl.solana.com
+        </Link>.
       </p>
     </main>
   );
